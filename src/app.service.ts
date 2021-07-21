1017c8daeeddce419660a5a55bda37953b2c3065
electro-wsl <joaovictordaijo@gmail.com>
2021-07-21 16:49:06 -0300

    Comment on app.service

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
     return 'Hello World!';
   }
 
+  // Add more methods like this, then save the file. Remove some of the methods then save the file etc.
   foo(): void {}
 
   foo2(): void {}
